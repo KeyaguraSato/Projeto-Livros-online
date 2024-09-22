64aaa48a82cba2d484da7b6e794c56c8b5b9ba90
KeyaguraSato <89804899+KeyaguraSato@users.noreply.github.com>
2024-09-22 17:05:35 -0300

    Primeiro update.
    Hospedando servidor.

---
+++ Livros/script.js
@@ -0,0 +1,41 @@
+const url = 'O_Paciente.pdf'; 
+let pdfDoc = null;
+let currentPage = 1;
+
+const canvas = document.getElementById('pdf-canvas');
+const ctx = canvas.getContext('2d');
+
+const renderPage = (num) => {
+    pdfDoc.getPage(num).then((page) => {
+        const viewport = page.getViewport({ scale: 1 });
+        canvas.width = viewport.width;
+        canvas.height = viewport.height;
+
+        const renderContext = {
+            canvasContext: ctx,
+            viewport: viewport,
+        };
+        page.render(renderContext);
+    });
+};
+
+const loadPdf = async () => {
+    pdfDoc = await pdfjsLib.getDocument(url).promise;
+    renderPage(currentPage);
+};
+
+document.getElementById('prev-page').addEventListener('click', () => {
+    if (currentPage > 1) {
+        currentPage--;
+        renderPage(currentPage);
+    }
+});
+
+document.getElementById('next-page').addEventListener('click', () => {
+    if (currentPage < pdfDoc.numPages) {
+        currentPage++;
+        renderPage(currentPage);
+    }
+});
+
+loadPdf();
